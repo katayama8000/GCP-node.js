703262c93cb7b07672c8fa2c2f5c31a49dea87a1
katayama8000 <tattu.0310@gmail.com>
2023-01-26 21:03:25 +0900

    update

---
+++ cloudrun-node.js/README.md
@@ -0,0 +1,9 @@
+[cliudrun に上げる手順](https://qiita.com/riku-shiru/items/d3f7dda5a5e87c4b26e9)
+
+[gcp docs](https://cloud.google.com/endpoints/docs/openapi/enable-api?hl=ja#gcloud)
+
+[secure manager](https://www.youtube.com/watch?v=JIE89dneaGo)
+
+---
+
+https://katayama8000-q4tcbuvapa-an.a.run.app/
--- cloudrun-node.js/index.js
+++ cloudrun-node.js/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log('Listening on port', port);
+});
